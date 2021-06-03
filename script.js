15937069ac57d681f3a9e5ea67dda232f6b32558
collinginsburg <chginsburg@email.wm.edu>
2021-06-03 16:18:57 -0700

    added check win functionality. Used switch case to test all combinations (8 win situations per player) as well as the tie situations (no win triggered but all places are not equal to null/they are all filled). A players win switched a player win variable to true, and this determines the message at the end. Also any win stops any new plays from being made.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const gameBoard = (() => {
 
         let index = e.target.getAttribute('data-index');
 
-        if (gameBoard.GAMEBOARDARRAY[index] === EMPTY){
+        if (gameBoard.GAMEBOARDARRAY[index] === EMPTY && gamePlay.getGameWon() === false){
             if (gamePlay.getP1turn() === true){
                 gameBoard.GAMEBOARDARRAY[index] = PLAYER1.getSymbol();
 
@@ -78,6 +78,15 @@ const gamePlay = (() => {
     let P1turn = true;
     let P2turn = false;
     let gameWon = false;
+    let P1win = false;
+    let P2win = false;
+    const getGameWon = () => {
+        if (gameWon === true){
+            return true
+        } else {
+            return false
+        }
+    }
     const getP1turn = () => {
         if (P1turn === true){
             return true;
@@ -100,13 +109,96 @@ const gamePlay = (() => {
         }
     }
     const checkScore = () => {
+        let gameboard = gameBoard.GAMEBOARDARRAY;
+        switch (true){
+        // player 1 win situations
+        case (gameboard[0] === PLAYER1.getSymbol() && gameboard[1] === PLAYER1.getSymbol() && gameboard[2] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+        case (gameboard[3] === PLAYER1.getSymbol() && gameboard[4] === PLAYER1.getSymbol() && gameboard[5] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+        case (gameboard[6] === PLAYER1.getSymbol() && gameboard[7] === PLAYER1.getSymbol() && gameboard[8] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+        case (gameboard[0] === PLAYER1.getSymbol() && gameboard[3] === PLAYER1.getSymbol() && gameboard[6] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+        case (gameboard[1] === PLAYER1.getSymbol() && gameboard[4] === PLAYER1.getSymbol() && gameboard[7] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+        case (gameboard[2] === PLAYER1.getSymbol() && gameboard[5] === PLAYER1.getSymbol() && gameboard[8] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+        case (gameboard[0] === PLAYER1.getSymbol() && gameboard[4] === PLAYER1.getSymbol() && gameboard[8] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+        case (gameboard[2] === PLAYER1.getSymbol() && gameboard[4] === PLAYER1.getSymbol() && gameboard[6] === PLAYER1.getSymbol()):
+            gameWon = true;
+            P1win = true;
+            break;
+    // player 2 win situations
+        case (gameboard[0] === PLAYER2.getSymbol() && gameboard[1] === PLAYER2.getSymbol() && gameboard[2] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+        case (gameboard[3] === PLAYER2.getSymbol() && gameboard[4] === PLAYER2.getSymbol() && gameboard[5] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+        case (gameboard[6] === PLAYER2.getSymbol() && gameboard[7] === PLAYER2.getSymbol() && gameboard[8] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+        case (gameboard[0] === PLAYER2.getSymbol() && gameboard[3] === PLAYER2.getSymbol() && gameboard[6] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+        case (gameboard[1] === PLAYER2.getSymbol() && gameboard[4] === PLAYER2.getSymbol() && gameboard[7] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+        case (gameboard[2] === PLAYER2.getSymbol() && gameboard[5] === PLAYER2.getSymbol() && gameboard[8] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+        case (gameboard[0] === PLAYER2.getSymbol() && gameboard[4] === PLAYER2.getSymbol() && gameboard[8] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+        case (gameboard[2] === PLAYER2.getSymbol() && gameboard[4] === PLAYER2.getSymbol() && gameboard[6] === PLAYER2.getSymbol()):
+            gameWon = true;
+            P2win = true;
+            break;
+    // tie situations
+        case (gameboard[0] != null && gameboard[1] != null && gameboard[2] != null && gameboard[3] != null && gameboard[4] != null && gameboard[5] != null && gameboard[6] != null && gameboard[7] != null && gameboard[8] != null):
+            gameWon = true;
+            break;
+        }
 
     }
     const play = () => {
         gameBoard.displayGameboard();
         checkScore();
         if (gameWon === true){
-
+            switch (true){
+                case (P1win === true):
+                    console.log(`${PLAYER1.getName()} wins`)
+                    break;
+                case (P2win === true):
+                    console.log(`${PLAYER2.getName()} wins`)
+                    break;
+                case (P1win === false && P2win === false):
+                    console.log(`TIE!`);
+                    break;
+            }
         } else {
             if (P1turn === true) {
                 P1turn = false;
@@ -121,7 +213,8 @@ const gamePlay = (() => {
         play,
         whoseTurn,
         getP1turn,
-        getP2turn
+        getP2turn,
+        getGameWon
         
     };
 })();
