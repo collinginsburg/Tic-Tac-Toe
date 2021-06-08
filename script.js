1b700a8f88f94d73181f61a4b8bfbebb6504c016
collinginsburg <chginsburg@email.wm.edu>
2021-06-07 18:24:27 -0700

    big commit. set up name input modals, and altered player factory logic to allow name to change. Couldnt think of another way to create a global player const to be used by the rest of the game objects. design is eh rn, will add arrow to indicate player's turn

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
+// ************************player factory function**************************************************
+
+const playerFactory = (playernumber, name, playersymbol) => {
+    const getPlayerNumber = () => playernumber;
+    const getSymbol = () => playersymbol;
+    // const getName  = () => name;
+    const info = () => console.log(`Player ${getPlayerNumber()} is named ${name} and they are playing as ${getSymbol()}`);
+    return{getPlayerNumber, getSymbol, info, name}
+}
+// initial player variables
+const PLAYER1 = playerFactory(1, "", "X");
+const PLAYER2 = playerFactory(2, "", "O");
+
+// ************************create players from user input MODULE?**********************************
+
+// function playerInputs(){
+//     PLAYER1.name = document.querySelector('#p1name').value;
+//     PLAYER2.name = document.querySelector('#p2name').value;
+// }
+
+
+// function displayPlayerNames(){
+//     let startoptions = document.querySelector('.startoptions');
+//     let namedisplay = document.querySelector('.namedisplay');
+//     let player1text =  document.querySelector('.player1');
+//     let player2text = document.querySelector('.player2');
+//     player1text.textContent = `${PLAYER1.name}: ${PLAYER1.getSymbol()}`;
+//     player2text.textContent = `${PLAYER2.name}: ${PLAYER2.getSymbol()}`;
+//     startoptions.style.display = 'none'; 
+//     namedisplay.style.opacity = '100';
+// }
+
+// function startGame(){
+//     playerInputs();
+//     displayPlayerNames();
+// }
+// const startgamebutton = document.querySelector('.startgame.btn');
+// startgamebutton.addEventListener('click', startGame);
+
+
+
+
+
+// ************************GAMEBOARD MODULE*********************************************************
+
 const gameBoard = (() => {
     const EMPTY = null;
     // const GAMEBOARDARRAY = ["X", "O", "X", "X", EMPTY, "X", "X", "O", "X"];
     const GAMEBOARDARRAY = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY];
 
+    const resetGameBoard = () => {
+        for (i=0; i<GAMEBOARDARRAY.length; i++){
+        GAMEBOARDARRAY[i] = EMPTY;
+        }
+        displayGameboard();
+    }
     const addSymboltoGameboardArray = (e) => {
 
         let index = e.target.getAttribute('data-index');
@@ -53,28 +104,72 @@ const gameBoard = (() => {
             grid.appendChild(square);
         }
     }
-
     return{
         displayGameboard,
         GAMEBOARDARRAY,
-        addSymboltoGameboardArray
+        addSymboltoGameboardArray,
+        resetGameBoard
     };
 
 })();
 
 
-const playerFactory = (playernumber, name, playersymbol) => {
-    const getPlayerNumber = () => playernumber;
-    const getSymbol = () => playersymbol;
-    const getName  = () => name;
-    const info = () => console.log(`Player ${getPlayerNumber()} is named ${getName()} and he is playing as ${getSymbol()}`);
-    return{getPlayerNumber, getName, getSymbol, info}
-}
 
-const PLAYER1 = playerFactory(1, "Collin", "X");
-const PLAYER2 = playerFactory(2, "Adam", "O");
+
+
+
+// ************************gameplay MODULE***********************************************************
 
 const gamePlay = (() => {
+    // initial player inputs for names
+    const playerInputs = () => {
+        PLAYER1.name = document.querySelector('#p1name').value;
+        PLAYER2.name = document.querySelector('#p2name').value;
+    }
+    const displayPlayerNames = () => {
+        let startoptions = document.querySelector('.startoptions');
+        let namedisplay = document.querySelector('.namedisplay');
+        let player1text =  document.querySelector('.player1');
+        let player2text = document.querySelector('.player2');
+        player1text.textContent = `${PLAYER1.name}`;
+        player2text.textContent = `${PLAYER2.name}`;
+        startoptions.style.display = 'none'; 
+        namedisplay.style.display = 'flex';
+    }
+    const displayPlayerInputPanel = () => {
+        let startoptions = document.querySelector('.startoptions');
+        let namedisplay = document.querySelector('.namedisplay');
+        startoptions.style.display = 'flex'; 
+        namedisplay.style.display = 'none';
+    }
+    const initialGameSettings = () => {
+        P1turn = true;
+        P2turn = false;
+        gameWon = false;
+        P1win = false;
+        P2win = false;
+    }
+    const startGame = () => {
+        waitingForInput = false;
+        playerInputs();
+        displayPlayerNames();
+        initialGameSettings();
+        gameBoard.resetGameBoard();
+    }
+
+    const resetGame = () => {
+        waitingForInput = true;
+        initialGameSettings();
+        displayPlayerInputPanel();
+        gameBoard.resetGameBoard();
+    }
+    const startgamebutton = document.querySelector('.startgame.btn');
+    startgamebutton.addEventListener('click', startGame);
+    const resetgamebutton = document.querySelector('.resetgame.btn');
+    resetgamebutton.addEventListener('click', resetGame);
+
+// initial game settings: p1 goes first, the game is not won yet so it can be played
+    let waitingForInput = true;
     let P1turn = true;
     let P2turn = false;
     let gameWon = false;
@@ -182,39 +277,41 @@ const gamePlay = (() => {
             gameWon = true;
             break;
         }
-
     }
     const play = () => {
-        gameBoard.displayGameboard();
-        checkScore();
-        if (gameWon === true){
-            switch (true){
-                case (P1win === true):
-                    console.log(`${PLAYER1.getName()} wins`)
-                    break;
-                case (P2win === true):
-                    console.log(`${PLAYER2.getName()} wins`)
-                    break;
-                case (P1win === false && P2win === false):
-                    console.log(`TIE!`);
-                    break;
-            }
-        } else {
-            if (P1turn === true) {
-                P1turn = false;
-                P2turn = true;
-            } else if (P2turn === true){
-                P1turn = true;
-                P2turn = false;
-            }
+        if (waitingForInput === false){
+                gameBoard.displayGameboard();
+                checkScore();
+                if (gameWon === true){
+                    switch (true){
+                        case (P1win === true):
+                            console.log(`${PLAYER1.name} wins`)
+                            break;
+                        case (P2win === true):
+                            console.log(`${PLAYER2.name} wins`)
+                            break;
+                        case (P1win === false && P2win === false):
+                            console.log(`TIE!`);
+                            break;
+                    }
+                } else {
+                    if (P1turn === true) {
+                        P1turn = false;
+                        P2turn = true;
+                    } else if (P2turn === true){
+                        P1turn = true;
+                        P2turn = false;
+                    }
+                }
         }
     };
+
     return{
         play,
         whoseTurn,
         getP1turn,
         getP2turn,
-        getGameWon
+        getGameWon,
         
     };
 })();
